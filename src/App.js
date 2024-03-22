5cb9364e255b24428b1e2a4aa4f07efe2f5f3f5b
Zakharov Sergey <iteco.zakharov@crpt.ru>
2024-03-22 08:00:05 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import {NewsItem} from "./newsitem/NewsItem";
+import {useEffect, useState} from "react";
+
+const initNews = [
+    {
+        id: 1,
+        title: 'Первая новость',
+        url: 'www.example.com',
+        username: 'Пользователь',
+        date: '10.10.11',
+        score: 100
+    },
+    {
+        id: 2,
+        title: 'Вторая новость',
+        url: 'www.example.com',
+        username: 'Администратор',
+        date: '11.01.12',
+        score: 1000
+    },
+    {
+        id: 3,
+        title: 'Третья новость',
+        url: 'www.example.com',
+        username: 'Гость',
+        date: '10.10.21',
+        score: 1
+    }
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const checkStorage = () => JSON.parse(window.localStorage.getItem('newsKey')) || initNews
+
+    const [news, setNews] = useState(checkStorage)
+    const [count, setCount] = useState(0)
+
+    let idCurrent = news.length + 1
+
+    const newsNew = {
+        id: idCurrent.toString(),
+        title: 'Новая новость',
+        url: 'www.example.com',
+        username: 'Пользователь',
+        date: '10.10.24',
+        score: 100
+    }
+
+    useEffect(() => {
+            window.localStorage.setItem('newsKey', JSON.stringify(news))
+        }, [news]
+    )
+
+    const newsCountHandler = () => {
+        idCurrent++
+        setNews((prevState) => [...prevState, newsNew])
+        console.log(newsNew.id)
+    }
+
+    return (
+        <>
+            <div>Количество новостей: {news.length}</div>
+            <button onClick={newsCountHandler}>Добавить новость</button>
+            <button onClick={() => setCount(count + 1)}>{count}</button>
+            {
+                news.map(item => {
+                    return <NewsItem
+                        key={item.id}
+                        title={item.title}
+                        url={item.url}
+                        username={item.username}
+                        date={item.date}
+                        points={item.score}
+                    />
+                })
+            }
+        </>
+    )
 }
 
 export default App;
---
+++ src/newsitem/NewsItem.js
@@ -0,0 +1,21 @@
+import style from './NewsItem.module.css'
+import {useState} from "react";
+
+export function NewsItem(props) {
+    return (
+        <div className={style.container}>
+            <a className={style.link} href={props.url}>{props.title}</a>
+
+            <div className={style.info}>
+                <div className={style.userData}>
+                    <span>{props.username} | </span>
+                    <span>{props.date}</span>
+                </div>
+
+                <div className={style.score}>
+                    {props.points} points  |
+                </div>
+            </div>
+        </div>
+    )
+}
